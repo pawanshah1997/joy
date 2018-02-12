88ef5fc590440e8198dd64ff8b98ff9c025da2ad
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2018-02-12 12:30:45 +0200

    fix ordering of checking add torrent param members to match libtorrent behaviour

--- src/core/Application/Application.js
+++ src/core/Application/Application.js
@@ -543,10 +543,10 @@ class Application extends EventEmitter {
     }
 
     // Add torrents paramaters should only have one of ti, url or infoHash
-    if (settings.metadata) {
-      params.ti = settings.metadata
-    } else if (settings.url) {
+    if (settings.url) {
       params.url = settings.url
+    else if (settings.metadata) {
+      params.ti = settings.metadata
     } else {
       params.infoHash = infoHash
     }
