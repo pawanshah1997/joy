7e1897c871f75d1affad19dd2c737b7c590567fa
Bedeho Mender <bedeho.mender@gmail.com>
2018-02-16 20:35:23 +0100

    Application: bug in NUMBER_OF_RESOURCE_TYPES
    Did not return value

--- src/core/Application/Application.js
+++ src/core/Application/Application.js
@@ -107,7 +107,7 @@ class Application extends EventEmitter {
     STORED_TORRENTS : 4
   }
 
-  static get NUMBER_OF_RESOURCE_TYPES() { Object.keys(Application.RESOURCE).length }
+  static get NUMBER_OF_RESOURCE_TYPES() { return Object.keys(Application.RESOURCE).length }
 
   /**
    * {Set.<RESOURCES>} The resources which are currently started
