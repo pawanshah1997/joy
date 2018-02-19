e3d157c5f644854a7266cd51c7d5c781f4634a99
Bedeho Mender <bedeho.mender@gmail.com>
2018-02-19 14:16:52 +0100

    enhance(TorrentTableRowStore): introduce viabilityOfPaidDownloadingTorrent
    See inline comment

--- src/scenes/Common/TorrentTableRowStore.js
+++ src/scenes/Common/TorrentTableRowStore.js
@@ -19,10 +19,11 @@ class TorrentTableRowStore {
    */
   torrentStore
   
-  constructor(torrentStore, applicationStore, showToolbar) {
+  constructor(torrentStore, applicationStore, walletStore, showToolbar) {
     
     this.torrentStore = torrentStore
     this._applicationStore = applicationStore
+    this._walletStore = walletStore
     this.setShowToolbar(showToolbar)
   }
   
@@ -52,6 +53,34 @@ class TorrentTableRowStore {
   openFolder() {
     shell.openItem(this.torrentStore.savePath)
   }
+  
+  /**
+   *
+   * NB: Possible hack note
+   *
+   * Its not clear that this should live here, because it will need to be
+   * computed different places in the UI stores hierarchy, which violates the "single
+   * source of truth" principle. On the other hand, we have no torrent level
+   * representation which would otherwise hold this. The `TorrentStore` has to
+   * match the `Torrent`, hence we would need ot add this to that class, but that also seems wrong
+   * A brighter mind should review this.
+   *
+   */
+  
+  @computed get
+  viabilityOfPaidDownloadingTorrent() {
+  
+    let balance = 0
+    let walletStarted = false
+  
+    if (this._walletStore) {
+      balance = this._walletStore.totalBalance
+      walletStarted = this._walletStore.state === Wallet.STATE.STARTED
+    }
+  
+    return computeViabilityOfPaidDownloadingTorrent(this.torrentStore.state, walletStarted, balance, this.torrentStore.viabilityOfPaidDownloadInSwarm)
+  }
+  
   
   @computed get
   playableMediaList() {
