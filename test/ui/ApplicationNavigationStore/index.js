ab86a30084d97f48388a4146fd574e0fe4be350e
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2018-02-21 00:08:08 +0200

    tests fix ApplicationNavigationStore

--- test/ui/ApplicationNavigationStore/index.js
+++ test/ui/ApplicationNavigationStore/index.js
@@ -4,11 +4,10 @@ var assert = require('chai').assert
 
 const createInitialValues = () => {
   return [
+    {}, // UIStore
     ApplicationNavigationStore.TAB.Downloading,
-    1,
+    1, // numberCompletedInBackground
     'USD',
-    { totalBalance: 100000 },
-    { cryptoToUsdExchangeRate: 10000 },
     100000000
   ]
 }
@@ -22,8 +21,8 @@ describe('ApplicationNavigationStore', function () {
   })
 
   it('constructor', function () {
-    assert.equal(navigationStore.activeTab, initialValues[0])
-    assert.equal(navigationStore.numberCompletedInBackground, initialValues[1])
-    assert.equal(navigationStore.fiatUnit, initialValues[2])
+    assert.equal(navigationStore.activeTab, initialValues[1])
+    assert.equal(navigationStore.numberCompletedInBackground, initialValues[2])
+    assert.equal(navigationStore.fiatUnit, initialValues[3])
   })
 })
