215bae17ff2f85663ac19eeae4999a28b9d2c9af
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2018-02-26 16:39:39 +0200

    main.js fix squirrel startup process

--- src/main.js
+++ src/main.js
@@ -4,96 +4,144 @@ const url = require('url')
 const isDev = require('electron-is-dev')
 const updater = require('./updater')
 const protocol = require('./protocol')
+const assert = require('assert')
 
-import { createTemplate } from './menu'
+import {createTemplate} from './menu'
 import {enableLiveReload} from 'electron-compile'
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win = null
 
-// This method makes your application a Single Instance Application -
-// instead of allowing multiple instances of your app to run,
-// this will ensure that only a single instance of your app is running
-var shouldQuit = app.makeSingleInstance(function(commandLine, workingDirectory) {
+function showMainWindow () {
+  if (win == null) return
 
-    // Someone tried to run a second instance, we should focus our window.
-    if (win) {
-        if (win.isMinimized()) win.restore()
-        win.focus()
-    }
-});
-
-if (shouldQuit) {
-    app.quit()
-} else {
-
-    // This method will be called when Electron has finished
-    // initialization and is ready to create browser windows.
-    // Some APIs can only be used after this event occurs.
-    app.on('ready', createWindow)
-
-    app.on('activate', () => {
-        // On macOS it's common to re-create a window in the app when the
-        // dock icon is clicked and there are no other windows open.
-        if (win === null) {
-            createWindow()
-        }
-    })
+  // delay for bug: https://github.com/electron/electron/issues/4338
+  setTimeout(function () {
+    if (win.isMinimized()) win.restore()
+    win.focus()
+    win.show()
+  }, 100)
 }
 
-// Listen to broadcast channel from main window
-ipcMain.on('main-window-channel', (event, arg) => {
+// First thing to check for is if this is an instace of an update installation
+// using the squirell updater framework (Windows and MacOS)
+if(require('electron-squirrel-startup')) {
+  // This is were we handle tasks relavent to install/update/removal of the application
+  app.quit()
 
-    if(arg === 'user-closed-app') {
+} else {
+  main()
+}
 
-        // Exit application
-        updater.quit()
-        app.quit()
-    }
+function main () {
 
-})
+  /*
+    This method makes your application a Single Instance Application -
+    instead of allowing multiple instances of your app to run,
+    this will ensure that only a single instance of your app is running.
+    argv array of string (command line arguments of second instance)
+    On macOS the system enforces single instance automatically when users
+    try to open a second instance of your app in Finder,
+    and the open-file and open-url events will be emitted for that.
+    However when users start your app in command line the system's
+    single instance mechanism will be bypassed and you have to use this method to ensure single instance.
+  */
+  const shouldQuit = app.makeSingleInstance(function (argv, workingDirectory) {
+      // Someone tried to run a second instance, we should focus our window.
+      showMainWindow()
 
-// Listen if we need to modify window size to fit video or going back to
-// application size
-ipcMain.on('set-bounds', (event, arg) => {
+      // TODO: inform main application window of second instance and argv
+      // This is where Windows and Linux will get arguments when launched for being a protocol handler
+  })
 
-    // verify if window exist and if we are not already in fullscreen
-    if (win && !win.isFullScreen()) {
-
-      // Set the new window size
-      win.setContentSize(arg.width, arg.height, true) // animate on Mac
+  if (shouldQuit) {
+    app.quit()
+    return
   }
-})
 
-// Listen if we need to block the save power feature to avooid the screen going
-// black in the middle of a video.
-ipcMain.on('power-save-blocker', (event, arg) => {
-    const {enable, disable} = require('./power-save-blocker')
+  // For MacOS
+  // TODO: capture and queue these events here. Main window will not likely be ready in time to
+  // handle earliest occurance of these events (when app is launching as a protocol handler)
+  app.on('open-file', showMainWindow)
+  app.on('open-url', showMainWindow)
 
-    if (arg.enable) {
-      // Enable blocker
-      enable()
-    } else {
-      // Disable blocker
-      disable()
+
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  // Some APIs can only be used after this event occurs.
+  app.on('ready', createMainWindow)
+
+  app.on('activate', () => {
+      // On macOS it's common to re-create a window in the app when the
+      // dock icon is clicked and there are no other windows open.
+      if (win === null) {
+        createMainWindow()
+      }
+  })
+
+  // Listen to broadcast channel from main window
+  ipcMain.on('main-window-channel', (event, arg) => {
+
+      if(arg === 'user-closed-app') {
+
+          // Exit application
+          updater.quit()
+          app.quit()
+      }
+  })
+
+  // Listen if we need to modify window size to fit video or going back to
+  // application size
+  ipcMain.on('set-bounds', (event, arg) => {
+
+      // verify if window exist and if we are not already in fullscreen
+      if (win && !win.isFullScreen()) {
+
+        // Set the new window size
+        win.setContentSize(arg.width, arg.height, true) // animate on Mac
     }
-})
+  })
 
-function createWindow () {
+  // Listen if we need to block the save power feature to avooid the screen going
+  // black in the middle of a video.
+  ipcMain.on('power-save-blocker', (event, arg) => {
+      const {enable, disable} = require('./power-save-blocker')
 
-    // Not really usefull for now :)
-    protocol.init()
+      if (arg.enable) {
+        // Enable blocker
+        enable()
+      } else {
+        // Disable blocker
+        disable()
+      }
+  })
 
-    if (isDev) {
+  // Not really usefull for now...
+  // protocol.init() // throwing "TypeError: protocol.registerStringProtocol is not a function"
 
-        console.log('2xenableLiveReload')
+  if (isDev) {
+    console.log('2xenableLiveReload')
 
-        // Enable live reloading : Needs to happen prior to `new BrowserWindow`
-        // https://github.com/electron/electron-compile/blob/master/README.md
-        enableLiveReload({strategy: 'react-hmr'})
-        enableLiveReload({strategy: 'react-hmr'})
-    }
+    // Enable live reloading : Needs to happen prior to `new BrowserWindow`
+    // https://github.com/electron/electron-compile/blob/master/README.md
+    enableLiveReload({strategy: 'react-hmr'})
+    enableLiveReload({strategy: 'react-hmr'})
+
+  } else {
+    crashReporter.start({
+      productName: "JoyStream",
+      companyName: "joystream",
+      submitURL: "https://joystream.sp.backtrace.io:6098/post?format=minidump&token=55e469c9e2258e7fd1b47a8ebd4bdc4ddc16b7521b18d3c34941fe186f660ca8",
+      uploadToServer: true
+    })
+
+  }
+}
+
+function createMainWindow () {
+
+  assert(win === null)
 
   // Create the browser window.
   win = new BrowserWindow({
@@ -134,13 +182,7 @@ function createWindow () {
     // Maximize window
     win.maximize()
 
-    // Handle squirrel event. Avoid calling for updates when install
-    if(require('electron-squirrel-startup')) {
-      app.quit()
-      // Hack because app.quit() is not immediate
-      process.exit(0)
-    }
-    // Check for updates
+    // Show Updater window and check for updates after main application window finished loading
     win.webContents.once("did-frame-finish-load", function (event) {
       updater.init()
     })
@@ -151,23 +193,14 @@ function createWindow () {
     }
   }
 
-
-
   // Load file for the app
-  var filename_to_load = process.env.COMPONENT_DEVELOPMENT_MODE ? 'component-development/index.html' : 'index.html'
+  const filename_to_load = process.env.COMPONENT_DEVELOPMENT_MODE ? 'component-development/index.html' : 'index.html'
 
   win.loadURL(url.format({
     pathname: path.join(__dirname, filename_to_load),
     protocol: 'file:',
     slashes: true
   }))
-
-  crashReporter.start({
-    productName: "JoyStream",
-    companyName: "joystream",
-    submitURL: "https://joystream.sp.backtrace.io:6098/post?format=minidump&token=55e469c9e2258e7fd1b47a8ebd4bdc4ddc16b7521b18d3c34941fe186f660ca8",
-    uploadToServer: true
-  })
 
   // Emitted when the window is closed.
   win.on('closed', () => {
