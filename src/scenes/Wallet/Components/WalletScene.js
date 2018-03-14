ccea5915299e48df1fb601ac0a8185dc27ba91c6
Bedeho Mender <bedeho.mender@gmail.com>
2018-03-14 12:56:28 -0700

    enhance(WalletScene): integrate new BCH information component

--- src/scenes/Wallet/Components/WalletScene.js
+++ src/scenes/Wallet/Components/WalletScene.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
 import { inject, observer } from 'mobx-react'
 import Wallet from '../../../core/Wallet'
+import BCHInformationNoticeSceneContent from './BCHInformationNoticeSceneContent'
 import LoadingWalletSceneContent from './LoadingWalletSceneContent'
 import LiveWalletSceneContent from './LiveWalletSceneContent'
 
 const WalletScene = inject('UIStore')(observer((props) => {
   
   let walletStore = props.UIStore.applicationStore.walletStore
-  
-  if(!walletStore || walletStore.state !== Wallet.STATE.STARTED)
+
+  if(props.UIStore.onboardingStore && props.UIStore.onboardingStore.showBCHNoticeInWallet)
+    return <BCHInformationNoticeSceneContent onAcceptClick={props.UIStore.onboardingStore.acceptBCHInformationNotice} />
+  else if(!walletStore || walletStore.state !== Wallet.STATE.STARTED)
     return <LoadingWalletSceneContent walletStore={walletStore}/>
   else
     return <LiveWalletSceneContent/>
