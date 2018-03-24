742c4fc97a5a32faaf4895e5440bc288150804de
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2018-03-23 21:58:03 -0700

    remove unused functions in application settings

--- src/core/ApplicationSettings/ApplicationSettings.js
+++ src/core/ApplicationSettings/ApplicationSettings.js
@@ -165,10 +165,6 @@ class ApplicationSettings extends EventEmitter {
     this._set(BITTORRENT_PORT, bittorrentPort)
   }
 
-  deleteBitTorrentPort() {
-    this._delete(BITTORRENT_PORT)
-  }
-
   defaultSellerTerms() {
     return this._get(DEFAULT_SELLER_TERMS)
   }
@@ -216,11 +212,6 @@ class ApplicationSettings extends EventEmitter {
 
   setClaimedFreeBCH(claimedFreeBCH) {
     this._set(CLAIMED_FREE_BCH, claimedFreeBCH)
-  }
-
-  deleteDefaultTerms () {
-    this._delete(DEFAULT_BUYER_TERMS)
-    this._delete(DEFAULT_SELLER_TERMS)
   }
 
   _delete (key) {
