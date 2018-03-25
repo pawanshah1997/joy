db1b0c898ce42f40e6008c8bab216c764229de13
Bedeho Mender <bedeho.mender@gmail.com>
2018-03-24 22:59:34 -0700

    update(UIStore): add support for new settings scene

--- src/scenes/UIStore.js
+++ src/scenes/UIStore.js
@@ -1,6 +1,6 @@
 import {observable, action, computed} from 'mobx'
 import assert from 'assert'
-import {shell} from 'electron'
+import {shell, remote} from 'electron'
 import open from 'open'
 var debug = require('debug')('UIStore')
 
@@ -782,6 +782,22 @@ class UIStore {
 
   })
 
+  // Hooks for {@link ApplicationSettings}
+
+  /**
+   * Handler for when download folder is changed in application settings
+   * @param downloadFolder
+   */
+  _onNewDownloadFolderAction = action((downloadFolder) => {
+
+    let applicationSettingsStore = this.applicationStore.applicationSettingsStore
+
+    assert(this.applicationStore.applicationSettingsStore)
+
+    applicationSettingsStore.setDownloadFolder(downloadFolder)
+
+  })
+
   @action.bound
   setCurrentPhase(currentPhase) {
     this.currentPhase = currentPhase
@@ -958,6 +974,32 @@ class UIStore {
       }
 
     })
+
+  }
+
+  @action.bound
+  choseNewDownloadFolder = () => {
+
+    // Make sure the main phase scene is active
+    if(this.alivePhaseScene !== UIStore.ALIVE_PHASE_SCENE.Main)
+      throw Error('Must be in the main alive phase scene.')
+
+    // Make sure we are on the right tab
+    if(this.applicationNavigationStore.activeTab !== ApplicationNavigationStore.TAB.Settings)
+      throw Error('Must be on the settings tab.')
+
+    // Allow user to picke folder using native dialog
+    let folderPicked = remote.dialog.showOpenDialog({
+      title: 'Pick folder where torrents will be stored',
+      properties: ['openDirectory']}
+    )
+
+    if (!folderPicked || folderPicked.length === 0) {
+      return
+    }
+
+    // Update download folder
+    this._application.applicationSettings.setDownloadFolder(folderPicked[0])
 
   }
 
