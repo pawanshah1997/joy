a0cdaa21427a93b496929125fe106adce40f832d
Bedeho Mender <bedeho.mender@gmail.com>
2018-03-28 15:50:14 -0700

    enhance(Seeding): introduce expaliner field about uploading prospects

--- src/scenes/Seeding/Seeding.js
+++ src/scenes/Seeding/Seeding.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { observer, inject } from 'mobx-react'
 import PropTypes from 'prop-types'
+import Radium from 'radium'
+import ReactTooltip from 'react-tooltip'
 
 import {
     LabelContainer,
@@ -26,6 +28,62 @@ function getStyles (props) {
     }
   }
 }
+
+const EarnMoneyExplainer = Radium((props) => {
+
+  let styles = {
+    explainerContainer: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    explainerTextDiv: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      fontSize: '14px',
+      backgroundColor: '#3e5c93',
+      color: '#a5b7d9',
+      paddingLeft: '15px',
+      paddingRight: '15px',
+      borderRadius: '40px',
+      height: '30px',
+
+      ':hover' :  {
+        color: 'white'
+      }
+    },
+    toolTipStyle: {
+      width: '350px',
+      fontSize: '18px',
+      paddingLeft: '10px',
+      paddingRight: '10px',
+      paddingTop: '18px',
+      paddingBottom: '18px'
+    }
+  }
+
+  return (
+    <div style={styles.explainerContainer}>
+      <span style={styles.explainerTextDiv}
+            data-tip
+            data-for={"explainer-tooltip"}>
+        When will I earn money?
+      </span>
+      <ReactTooltip id={"explainer-tooltip"}
+                    place='bottom'
+                    effect='solid'
+                    multiline={true}
+      >
+        <div style={styles.toolTipStyle}>
+          Earning money may take time as it requires both that buyers are joining the torrent swarm to buy, and that you are picked out for the trade among all the sellers that are currently available on the same swarm.
+
+          <span style={{fontWeight : 'bold', paddingLeft: '5px'}}>Be patient.</span>
+        </div>
+      </ReactTooltip>
+    </div>
+  )
+})
 
 const Seeding = inject('UIStore')(observer((props) => {
 
@@ -61,6 +119,12 @@ const Seeding = inject('UIStore')(observer((props) => {
            {...labelColorProps} />
            **/
           }
+
+          { /** Explainer **/}
+
+          <EarnMoneyExplainer />
+
+
 
           <CurrencyLabel tooltip={'Total revenue from uploads'}
                          satoshies={props.UIStore.totalRevenueFromPieces}
