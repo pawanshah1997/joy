cc2a64e82cb503e37359e0469e3141c2c99ef4a4
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2018-04-01 23:00:58 -0700

    only extract icon.png if it doesn't exist

--- src/defaultAppDirectory.js
+++ src/defaultAppDirectory.js
@@ -12,8 +12,15 @@ mkdirp.sync(appDirectory)
 
 // When native code (outside electron) needs resources that are packaged in the app .asar
 // file we copy it to the resources/ dir under the users' .joystream directory
-fs.createReadStream(path.join(__dirname, 'assets', 'appicon', 'icon.png'))
-  .pipe(fs.createWriteStream(path.join(appDirectory, 'icon.png')))
+// This is not really best place for this, but since there is no cross platform way to run code
+// on first run, it will have to do.
+const destinationIconPath = path.join(appDirectory, 'icon.png')
+const sourceIconPath = path.join(__dirname, 'assets', 'appicon', 'icon.png')
+
+if (!fs.existsSync(destinationIconPath)) {
+  fs.createReadStream(sourceIconPath)
+    .pipe(fs.createWriteStream(destinationIconPath))
+}
 
 
 module.exports = function () {
