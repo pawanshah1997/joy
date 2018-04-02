c92386a02762a5c26ba6a26a48d9a284bf9d51c8
Mokhtar Naamani <mokhtar.naamani@gmail.com>
2018-04-01 21:29:37 -0700

    extract app icon.png from asar when creating users .joystream home directory

--- src/defaultAppDirectory.js
+++ src/defaultAppDirectory.js
@@ -1,5 +1,6 @@
 import os from 'os'
 import path from 'path'
+import fs from 'fs'
 import mkdirp from 'mkdirp'
 import isDev from 'electron-is-dev'
 
@@ -8,6 +9,12 @@ import isDev from 'electron-is-dev'
 const appDirectory = path.join(os.homedir(), isDev ? '.joystream_development' : '.joystream')
 
 mkdirp.sync(appDirectory)
+
+// When native code (outside electron) needs resources that are packaged in the app .asar
+// file we copy it to the resources/ dir under the users' .joystream directory
+fs.createReadStream(path.join(__dirname, 'assets', 'appicon', 'icon.png'))
+  .pipe(fs.createWriteStream(path.join(appDirectory, 'icon.png')))
+
 
 module.exports = function () {
   return appDirectory
